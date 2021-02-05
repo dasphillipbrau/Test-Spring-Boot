b661214fcb3208bafe391c558b52b20850433112
Isaac Garro <leggyface@gmail.com>
2021-02-05 12:57:36 -0600

    Initial Upload

---
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -0,0 +1,18 @@
+package com.pluralsight.conferencedemo.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PersistenceConfiguration {
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:mysql://localhost:3306/spring_boot_app");
+        System.out.println("Custom DB initiated");
+        return builder.build();
+    }
+}
